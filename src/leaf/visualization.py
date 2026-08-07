ddec7fc5d2119d06df4350a36ef81d4f18dee2d4
Jonas Anderegg <jonas.anderegg@agroscope.admin.ch>
2026-08-07 12:56:17 +0200

    increased to max_workers = 2

--- src/leaf/visualization.py
+++ src/leaf/visualization.py
@@ -321,7 +321,7 @@ class Visualizer:
                 self.save_visualization(str(Path(data_set['rgb']).stem), img_bgr, 'symptoms_seg/vis')
 
         # max_workers = max(1, min(len(data), (__import__('os').cpu_count() or 1)))
-        max_workers = 1
+        max_workers = 2
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
             futures = [executor.submit(_process_one, data_set) for data_set in data]
             for future in tqdm(as_completed(futures), total=len(futures)):
