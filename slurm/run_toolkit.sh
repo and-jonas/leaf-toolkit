4bf0c5f5dbb2a37ab7a19c2d3c3de8f4821d47ea
and-jonas <jonas.anderegg@agroscope.admin.ch>
2026-08-19 11:03:50 +0000

    added slurm scriptls

---
+++ slurm/run_toolkit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#SBATCH --job-name=sshd
+#SBATCH --time=04:00:00
+#SBATCH --output=%x_%j_%N.log
+#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=16
+
+PORT=$(python3 -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
+
+echo "NODE: $(hostname)"
+echo "PORT: $PORT"
+echo "HOME: $HOME"
+
+cd ~/leaf-toolkit
+source .venv/bin/activate
+
+python test_canopy.py
+
+
+# start sshd on compute node
+/usr/sbin/sshd -D -p ${PORT} \
+  -f /dev/null \
+  -h ${HOME}/.ssh/sshd_host_key \
+  -o AuthorizedKeysFile=${HOME}/.ssh/authorized_keys
